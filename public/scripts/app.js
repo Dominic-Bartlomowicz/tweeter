5b85cd555195b270f2ab3e06b99dec9bbc26f606
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-03-28 20:30:37 -0700

    fourth commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,52 +1,8 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
-
 $(function () {
+
+  $(".button").click(function(){
+      $(".new-tweet").show(1000);
+  });
 
   $('.new-tweet').find('input').on('click', function(event) {
     event.preventDefault()
@@ -55,8 +11,12 @@ $(function () {
     console.log(data);
     // window.location.reload();
 
+    if($(".tweet-input").val().length == 0) {
+      alert("Please enter text!");
+    }
+
     if($(".tweet-input").val().length > 140) {
-      alert("Please shorten your tweet!");
+      alert("Please shorten your tweet! (140 Characters Maximum)");
     }
 
   });
@@ -137,6 +97,8 @@ $(function () {
        success: function (moreTweetsHtml){
        console.log('Success: ', moreTweetsHtml);
        loadTweets();
+       $('.tweet-input').val('');
+       $('.counter').text("140");
        }
      });
  }
